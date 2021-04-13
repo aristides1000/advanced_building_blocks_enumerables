6f0253e8cb57b73688038e5f431ebbfdc5ec4aa5
aristides1000 <aristides1000@gmail.com>
2021-04-13 19:21:59 -0400

    Build method my_each

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,6 +1,21 @@
+# https://github.com/ruby/ruby
+# https://www.ruby-lang.org/en/
+# https://ruby-doc.org/core-2.7.0/Array.html
+
 module Enumerable
   def my_each
-    # your code here
+    return self.dup unless block_given?
+
+    element = self
+
+    i = 0
+    loop do
+      yield(element[i])
+      i += 1
+      break if i == element.length
+    end
+    self
+
   end
 
   def my_each_with_index
@@ -34,4 +49,24 @@ module Enumerable
   def my_inject
     # your code here
   end
+
+  def test
+    my_each { |i, e, o| p "#{i} #{e} #{o}" }
+  end
+
+  # test
+
 end
+
+array = [1, 2, 3]
+
+p array.my_each
+
+p array.my_each { |n| p n * 2 }
+
+
+# Para ejecutar este archivo en irb, debo hacer lo siguiente 
+=begin
+$ irb
+>> load './filename.rb'
+=end
